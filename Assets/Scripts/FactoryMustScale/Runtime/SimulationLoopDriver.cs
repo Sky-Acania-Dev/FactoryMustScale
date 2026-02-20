9759cc802ae686228e41fdbc6bf406a47913623c
Sky_AcaniaDev <88903905+Sky-Acania-Dev@users.noreply.github.com>
2026-02-19 20:56:32 -0500

    chore: remove manually generated meta files

--- Assets/Scripts/FactoryMustScale/Runtime/SimulationLoopDriver.cs
+++ Assets/Scripts/FactoryMustScale/Runtime/SimulationLoopDriver.cs
@@ -1,4 +1,6 @@
 using FactoryMustScale.Simulation.Core;
+using FactoryMustScale.Simulation.Domains.Factory.Systems.Transport;
+using FactoryMustScale.Simulation.Item;
 using UnityEngine;
 
 namespace FactoryMustScale.Runtime
@@ -10,12 +12,36 @@ namespace FactoryMustScale.Runtime
     {
         private const float UnitTickDeltaSeconds = 1.0f / 32.0f;
 
-        private readonly ISimPhaseSystem[] _systems = System.Array.Empty<ISimPhaseSystem>();
+        [SerializeField]
+        private bool _useFactoryTransportLegacyAdapter;
+
+        private ISimPhaseSystem[] _systems = System.Array.Empty<ISimPhaseSystem>();
         private SimLoop _simLoop;
         private float _accumulatorSeconds;
 
+        public void ConfigureFactoryTransportState(in FactoryCoreLoopState state)
+        {
+            _systems = new ISimPhaseSystem[]
+            {
+                new FactoryTransportLegacyAdapter(in state),
+            };
+        }
+
         private void Awake()
         {
+            if (_systems.Length == 0 && _useFactoryTransportLegacyAdapter)
+            {
+                FactoryCoreLoopState initialFactoryState = new FactoryCoreLoopState
+                {
+                    ItemTransportAlgorithm = ItemTransportAlgorithm.SimplePush,
+                };
+
+                _systems = new ISimPhaseSystem[]
+                {
+                    new FactoryTransportLegacyAdapter(in initialFactoryState),
+                };
+            }
+
             _simLoop = new SimLoop(_systems);
             _accumulatorSeconds = 0.0f;
         }
